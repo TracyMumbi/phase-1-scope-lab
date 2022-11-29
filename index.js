7948b28499bb91b17f6b150b0c2d09cafa41d0b6
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2022-11-29 14:47:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+
+function upperCaseCustomerName() {
+        customerName = customerName.toUpperCase();
+        return customerName;
+    }
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'Susan';
+    leastFavoriteCustomer = 'jane';
+    return leastFavoriteCustomer;
+}
+
+
+
